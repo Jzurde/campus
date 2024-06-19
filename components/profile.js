19cb057c5c42c16cd67245482fb51ed0137cd269
Jzurde <jzurde@users.noreply.github.com>
2024-06-19 21:31:32 +0900

    added some layoutal fixes

--- components/profile.js
+++ components/profile.js
@@ -1,13 +1,14 @@
-import ProfileLogo from '@/public/profile.svg'
 import LinkButton from './link'
 import styles from'@/styles/profile.module.css'
 import { siteData } from '@/lib/constant'
 import parse from 'html-react-parser'
+import Image from 'next/image'
 
 export default function Profile(){
+    const profile_logo = siteData.profile.logo
     return(
         <div className={styles.container}>
-            <ProfileLogo width={175} />
+            <Image src={profile_logo.url} width={profile_logo.width} height={profile_logo.height}/>
             <h4>{siteData.profile.name}</h4>
             <p>{parse(siteData.profile.description)}</p>
             <LinkButton title="お問合せ" link='/contact'/>
