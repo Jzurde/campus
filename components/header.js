7c1e28f049063c7652f91a185eb96941c2cd04df
大石陸斗 <rikutooishi0911@gmail.com>
2024-11-22 22:38:51 +0900

    deleted header logo, added new logo generating components

--- components/header.js
+++ components/header.js
@@ -1,16 +1,21 @@
-import Logo from '../public/logo.svg'
 import styles from '@/styles/header.module.css'
 import Container from './container'
 import Link from 'next/link'
 import { siteMeta } from '@/lib/constant'
+import { Bungee } from 'next/font/google';
+
+const BungeeFont = Bungee({ subsets: ['latin'], weight: '400' });
 
 export default function Header() {
+    const logoText = siteMeta.siteLogo;
+    const isEnglish = /^[A-Za-z\s]+$/.test(logoText);
+
     return (
         <header className={styles.header}>
             <Container>
                 <div className={styles.inner}>
                     <Link href="/">
-                        <Logo width={180} />
+                        <h1 className={`${styles.logo_title} ${isEnglish ? BungeeFont.className : ''}`}>Campus</h1>
                     </Link>
                     <h4>{siteMeta.siteTitle}</h4>
                     <p className={styles.subcaption}>{siteMeta.siteDesc}</p>
