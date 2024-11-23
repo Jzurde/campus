721c6a650c57faeeb02f42182760baf8a4a4fc9d
Rikuto Oishi <rikutooishi0911@gmail.com>
2024-11-23 23:55:38 +0900

    extracted logo fonts to _constant.js

--- components/profile.js
+++ components/profile.js
@@ -1,19 +1,17 @@
 import LinkButton from './link'
-import styles from'@/styles/profile.module.css'
-import { siteData } from '@/lib/constant'
+import styles from '@/styles/profile.module.css'
 import parse from 'html-react-parser'
-import { Bungee } from 'next/font/google';
-import { _myProfile } from './_constant';
+import { _myProfile, logoFontInstance } from './_constant';
 
-const LogoFont = Bungee({ subsets: ['latin'], weight: '400' });
+const LogoFont = logoFontInstance;
 
-export default function Profile(){
-    return(
+export default function Profile() {
+    return (
         <div className={styles.container}>
             <h2 className={`${styles.profile_name} ${LogoFont.className}`}>{_myProfile.english_name}</h2>
             <h4>{_myProfile.name}</h4>
-            <p>{parse(siteData.profile.description)}</p>
-            <LinkButton title="お問合せ" link='/contact'/>
+            <p>{parse(_myProfile.description)}</p>
+            <LinkButton title="お問合せ" link='/contact' />
         </div>
     )
 }
