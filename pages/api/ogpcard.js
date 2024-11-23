74b59c1e9046def8fe77abefc85f5503296fe185
Rikuto Oishi <rikutooishi0911@gmail.com>
2024-11-23 23:25:53 +0900

    updated apis to refer to the new constant.js in component folder

--- pages/api/ogpcard.js
+++ pages/api/ogpcard.js
@@ -1,16 +1,14 @@
-import { siteData, siteMeta } from "@/lib/constant";
+import { _siteData } from "@/components/_constant";
+import { siteData } from "@/lib/constant";
 import { ImageResponse } from "next/og";
 
 export const config = {
     runtime: "edge",
 }
 
-const font = fetchFont()
-
-export async function fetchFont() {
-
+// Noto Sans JP フォントを取得
+const fetchFontNotoSansJP = async () => {
     const googleFontsUrl = 'https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@700&display=swap';
-
     const css = await (
         await fetch(googleFontsUrl, {
             headers: {
@@ -20,28 +18,42 @@ export async function fetchFont() {
         })
     ).text();
 
-    const resource = css.match(
-        /src: url\((.+)\) format\('(opentype|truetype)'\)/
-    );
-
+    const resource = css.match(/src: url\((.+)\) format\('(opentype|truetype)'\)/);
     if (!resource) return null;
     const res = await fetch(resource[1]);
     return res.arrayBuffer();
-}
+};
+
+// Bungee フォントを取得
+const fetchFontBungee = async () => {
+    const googleFontsUrl = 'https://fonts.googleapis.com/css2?family=Bungee&display=swap';
+    const css = await (
+        await fetch(googleFontsUrl, {
+            headers: {
+                "User-Agent":
+                    "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_8; de-at) AppleWebKit/533.21.1 (KHTML, like Gecko) Version/5.0.5 Safari/533.21.1",
+            },
+        })
+    ).text();
+
+    const resource = css.match(/src: url\((.+)\) format\('(opentype|truetype)'\)/);
+    if (!resource) return null;
+    const res = await fetch(resource[1]);
+    return res.arrayBuffer();
+};
 
 export default async function OGImageHandler(req) {
     try {
-        const { searchParams } = new URL(req.url)
-        const fontData = await font;
+        const { searchParams } = new URL(req.url);
+        const notoSansJP = await fetchFontNotoSansJP();
+        const bungee = await fetchFontBungee();
 
-        const hasTitle = searchParams.has('title')
-        const hasCategories = searchParams.has('categories')
-        const title = hasTitle ? searchParams.get('title').slice(0, 100) : siteData.shortDescription
-        const category = hasCategories ? searchParams.get('categories') : ""
+        const hasTitle = searchParams.has('title');
+        const hasCategories = searchParams.has('categories');
+        const title = hasTitle ? searchParams.get('title').slice(0, 100) : siteData.shortDescription;
+        const category = hasCategories ? searchParams.get('categories') : "";
 
-        const categories = category.split(',')
-
-
+        const categories = category.split(',');
 
         return new ImageResponse(
             (
@@ -59,7 +71,7 @@ export default async function OGImageHandler(req) {
                         borderRadius: '32px 32px 0 0',
                         flexDirection: 'column',
                         display: 'flex',
-                        boxShadow: '0px 0px 50px 0px #b0b0b0'
+                        boxShadow: '0px 0px 50px 0px #b0b0b0',
                     }}>
                         <div style={{
                             width: '100%',
@@ -69,37 +81,37 @@ export default async function OGImageHandler(req) {
                             justifyContent: 'flex-start',
                             alignItems: 'center',
                             gap: 22,
-                            borderBottom: 'solid 2px #eeecec'
+                            borderBottom: 'solid 2px #eeecec',
                         }}>
-                            <div style={{
-                                width: `${siteData.siteLogoSVGData.width}px`,
-                                height: `${siteData.siteLogoSVGData.height}px`,
-                                backgroundImage: `url(${`data:image/svg+xml,${encodeURIComponent(siteData.siteLogoSVGData.pure)}`})`,
-                                backgroundRepeat: 'no-repeat',
-                                backgroundSize: `${siteData.siteLogoSVGData.width}px ${siteData.siteLogoSVGData.height}px`,
-                            }}></div>
+                            <h1 style={{
+                                fontFamily: 'Bungee',
+                                fontSize: '35px',
+                                marginTop: 35,
+                            }}>{_siteData.siteLogo}</h1>
                             <p style={{
-                                fontSize: 20
-                            }}>{siteMeta.siteTitle}</p>
+                                fontSize: 20,
+                                fontFamily: 'NotoSansJP',
+                            }}>{_siteData.siteTitle}</p>
                             <p style={{
-                                fontSize: 15
-                            }}>{siteMeta.siteDesc}</p>
+                                fontSize: 15,
+                                fontFamily: 'NotoSansJP',
+                            }}>{_siteData.siteDesc}</p>
                         </div>
                         <div style={{
                             width: '100%',
                             padding: '51px 76px',
-                            display: 'flex'
+                            display: 'flex',
                         }}>
                             <div style={{
                                 display: 'flex',
                                 flexDirection: 'column',
-                                width: '100%'
+                                width: '100%',
                             }}>
                                 <div style={{
                                     display: 'flex',
                                     justifyContent: 'flex-start',
                                     alignItems: 'center',
-                                    gap: 16
+                                    gap: 16,
                                 }}>
                                     {
                                         categories.map((value) => {
@@ -110,7 +122,8 @@ export default async function OGImageHandler(req) {
                                                     fontSize: '32px',
                                                     lineHeight: '32px',
                                                     borderRadius: 16,
-                                                    display: 'flex'
+                                                    display: 'flex',
+                                                    fontFamily: 'NotoSansJP',
                                                 }}>
                                                     # {value}
                                                 </div>
@@ -121,7 +134,8 @@ export default async function OGImageHandler(req) {
                                 <h1 style={{
                                     color: '#393939',
                                     fontSize: '64px',
-                                    lineHeight: '72px'
+                                    lineHeight: '72px',
+                                    fontFamily: 'NotoSansJP',
                                 }}>{title}</h1>
                             </div>
                         </div>
@@ -134,16 +148,20 @@ export default async function OGImageHandler(req) {
                 fonts: [
                     {
                         name: 'NotoSansJP',
-                        data: fontData,
+                        data: notoSansJP,
+                        style: "normal",
+                    },
+                    {
+                        name: 'Bungee',
+                        data: bungee,
                         style: "normal",
                     }
                 ],
             }
-        )
+        );
 
-    }
-    catch (e) {
-        console.log('===OGImageHandler===')
-        console.log(e)
+    } catch (e) {
+        console.log('===OGImageHandler===');
+        console.log(e);
     }
 }
