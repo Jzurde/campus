560e2d6324c425bf514f2462eaa9afddf49f3754
Jzurde <jzurde@users.noreply.github.com>
2025-06-28 14:51:24 +0900

    プレビューモードに関するapi機能をapp/apiに移植

---
+++ app/api/preview/route.ts
@@ -0,0 +1,33 @@
+// app/api/preview/route.ts
+import { client } from "@/lib/cms-api";
+import { NextResponse } from "next/server";
+import { draftMode } from "next/headers";
+
+export async function GET(req: Request) {
+    const { searchParams } = new URL(req.url);
+    const draftKey = searchParams.get("draftKey");
+    const contentId = searchParams.get("contentId");
+
+    if (!draftKey || !contentId) {
+        return NextResponse.json({ message: "Missing query parameters" }, { status: 400 });
+    }
+
+    let data;
+    try {
+        data = await client.get({
+            endpoint: "posts",
+            contentId,
+            queries: { draftKey }
+        });
+    } catch (e) {
+        return NextResponse.json({ message: "Invalid slug" }, { status: 401 });
+    }
+
+    (await draftMode()).enable();
+
+    const response = NextResponse.redirect(new URL(`/posts/${data.id}`, req.url), {
+        status: 307,
+    });
+
+    return response;
+}
