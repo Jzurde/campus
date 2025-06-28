2c81907aa98531bd5e3b0f1bd88104427a32c5d3
Jzurde <jzurde@users.noreply.github.com>
2025-06-28 14:57:27 +0900

    pageディレクトリを削除し、いくつかの軽微なバグを修正

--- app/posts/[slug]/[page]/page.tsx
+++ app/posts/[slug]/[page]/page.tsx
@@ -71,10 +71,8 @@ export async function generateStaticParams() {
 export default async function Post({ params }: {
     params: { slug: string; page: string };
 }) {
-    console.log("PAGEPAGE!")
-    console.log(`page: ${await params.page}`)
     const { slug } = await params
-    const page = parseInt(await params.page, 10) || 1
+    const page = parseInt((await params).page, 10) || 1
 
 
     const { isEnabled: isInPreviewMode } = await draftMode()
