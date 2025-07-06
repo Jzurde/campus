8b74254b450c3d4b26303230103070c5a4a9c930
大石陸斗 <rikutooishi0911@gmail.com>
2025-07-06 09:37:31 +0900

    トップページの各見出しをmicroCMSで変更できるように修正

--- components/works/works.tsx
+++ components/works/works.tsx
@@ -1,14 +1,18 @@
+import { getSiteSettings } from '@/lib/cms-api'
 import LinkButton from '../link_button/link_button'
 import WorkList from '../work_list.js/work_list'
 import styles from './works.module.css'
 
-export default function Works({ works }: { works: any }) {
+export default async function Works({ works }: { works: any }) {
+    const settings = (await getSiteSettings("topPage")).topPage
+    const section_works = settings?.hasOwnProperty('section_works') ? settings.section_works : 'ワークス'
+    const subsection_works = settings?.hasOwnProperty('subsection_works') ? settings.subsection_works : '最近の作品'
     return (
         <section className={styles.container}>
-            <h2>ワークス</h2>
+            <h2>{section_works}</h2>
             <div className={styles.inner}>
                 <div className={styles.title_flex}>
-                    <h3>最近の作品</h3>
+                    <h3>{subsection_works}</h3>
                     <LinkButton title="全ての作品" type={1} link='/works' />
                 </div>
                 <WorkList works={works} />
