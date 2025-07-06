76bc6776d89e838b4198a67f85fecb0fb0e7fad2
Jzurde <jzurde@users.noreply.github.com>
2025-07-06 22:46:18 +0900

    問合せフォーム関連で初期値等をmicroCMSで設定できるように変更

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,6 +1,7 @@
 import { Metadata } from 'next';
 import ContactForm from '@/components/contact_form/contact_form';
 import Meta from '@/lib/meta';
+import { getSiteSettings } from '@/lib/cms-api';
 
 export async function generateMetadata() {
     return Meta({
@@ -9,6 +10,20 @@ export async function generateMetadata() {
     })
 }
 
-export default function Contact() {
-    return <ContactForm />
+export default async function Contact() {
+    
+    const settings = (await getSiteSettings("contactPage")).contactPage
+    const description_html = settings.hasOwnProperty('description') ? settings.description : "<p>お問合せフォームは以下です。<br>内容を確認の上、担当者より折り返しご連絡させていただきます。</p>"
+    const name_plaiceholder = settings.hasOwnProperty('name_plaiceholder') ? settings.name_plaiceholder : "お名前"
+    const email_plaiceholder = settings.hasOwnProperty('email_plaiceholder') ? settings.email_plaiceholder : "address@email.com"
+    const message_plaiceholder = settings.hasOwnProperty('message_plaiceholder') ? settings.message_plaiceholder : "お問い合わせ内容"
+
+    return (
+        <ContactForm 
+            description_html={description_html} 
+            name_plaiceholder={name_plaiceholder}
+            email_plaiceholder={email_plaiceholder}
+            message_plaiceholder={message_plaiceholder}
+            />
+    )
 }
